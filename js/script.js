3e6831913f3f724251afc1601b2ff7a85383791b
Jane Claire <pixiephreak@Janes-MBP.home>
2017-01-08 00:42:39 -0500

    satisfy requirments change 2 by adding more bears(cats changed to bears), addressing DRY violations by using loops and populating html dynamically instead of hardcoding elems. event listener needs to be fixed by using IFFY

--- js/script.js
+++ js/script.js
@@ -1,20 +1,52 @@
-// console.log("linked");
-var cat1 = "kitty";
-var cat2 = "cat";
+var cat1 = "https://placebear.com/100/100";
+var cat2 = "https://placebear.com/200/200";
+var cat3 = "https://placebear.com/300/300";
+var cat4 = "https://placebear.com/400/400";
+var cat5 = "https://placebear.com/500/500";
 
-$(function() {
-    $( "#cat1" ).text(cat1);
-  });
-$(function() {
-    $( "#cat2" ).text(cat2);
-  });
 
-var clicks = 0;
+var cats = [cat1,cat2,cat3,cat4,cat5];
+
+var clicks = function createVariables(){
+  var counts = [];
+
+  for (var i = 0; i <= 20; ++i) {
+      counts[i] = 0;
+  }
+
+  return counts;
+}();
+
 console.log(clicks);
 
-$('.cat').click(function() {
-	clicks +=1;
-	$('#counter').text('CAT CLICK:'+clicks);
-	// console.log('clicked');
+// Let's loop over the numbers in our array
+for (var i = 0; i < cats.length; i++) {
 
-});
+    // This is the number we're on...
+    var cat = cats[i];
+    console.log(cat);
+    // We're creating a DOM element for the pic and counter
+    var pic = document.createElement('div');
+    var src = '<img src='+'"'+cat+'"'+'/>'
+    console.log(src)
+    $(pic).prepend(src);
+
+    var h1 = '<h1 id="counter'+[i+1]+'"'+'>'+'</h1>';
+    console.log(h1);
+    $(pic).prepend(h1);
+
+    // ... and when we click, increment click num
+
+    pic.addEventListener('click', (function() {
+        clicks[i]+=1;
+        $('#counter'+[i]).text("BEAR COUNT:"+clicks[i]);
+    }));
+
+    // finally, let's add this element to the document
+    document.body.appendChild(pic);
+
+};
+
+
+
+
