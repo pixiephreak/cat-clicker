b05cd9e0f2a7651f3656b62fef8bf4f832f9e179
Jane Claire <pixiephreak@Janes-MBP.home>
2017-01-07 22:29:16 -0500

    updated project requirements: add cat and both display cat names

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
 // console.log("linked");
+var cat1 = "kitty";
+var cat2 = "cat";
 
+$(function() {
+    $( "#cat1" ).text(cat1);
+  });
+$(function() {
+    $( "#cat2" ).text(cat2);
+  });
 
 var clicks = 0;
 console.log(clicks);
 
-$('#cat').click(function() {
+$('.cat').click(function() {
 	clicks +=1;
 	$('#counter').text('CAT CLICK:'+clicks);
 	// console.log('clicked');
